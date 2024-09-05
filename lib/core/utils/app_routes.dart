5f7ebb2c580737a835ea84a6b48882a0f04ce9b9
Menna <mennaragab220@gmail.com>
2024-09-05 05:25:02 +0300

    book details app bar

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -1,9 +1,11 @@
 import 'package:go_router/go_router.dart';
+import 'package:my_books_app/features/home/presentation/views/book_details_view.dart';
 import 'package:my_books_app/features/home/presentation/views/home_view.dart';
 import 'package:my_books_app/features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRoutes {
   static const home = '/homeView';
+  static const bookdetails = '/bookdetails';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -13,6 +15,10 @@ abstract class AppRoutes {
       GoRoute(
         path: home,
         builder: (context, state) => HomeView(),
+      ),
+      GoRoute(
+        path: bookdetails,
+        builder: (context, state) => BookDetailsView(),
       ),
     ],
   );
--- lib/features/home/presentation/views/widgets/best_seller_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:my_books_app/core/utils/app_routes.dart';
 import 'package:my_books_app/core/utils/assets.dart';
 import 'package:my_books_app/core/utils/styles.dart';
 import 'package:my_books_app/features/home/presentation/views/widgets/rating_item.dart';
@@ -10,50 +12,56 @@ class BestSellerItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.15,
-          child: AspectRatio(
-            aspectRatio: 2.8 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  image: DecorationImage(
-                      image: Image.asset(Assets.test).image, fit: BoxFit.fill)),
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRoutes.bookdetails);
+      },
+      child: Row(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.15,
+            child: AspectRatio(
+              aspectRatio: 2.8 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    image: DecorationImage(
+                        image: Image.asset(Assets.test).image,
+                        fit: BoxFit.fill)),
+              ),
             ),
           ),
-        ),
-        const SizedBox(
-          width: 30,
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.height * 0.3,
-                child: const Text('Harry Potter and the order of the phoenix',
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                    style: Styles.textStyle20),
-              ),
-              const Text('the author', style: Styles.textStyle14),
-              Row(
-                children: [
-                  Text(
-                    '19.99 €',
-                    style: Styles.textStyle20
-                        .copyWith(fontWeight: FontWeight.bold),
-                  ),
-                  const Spacer(),
-                  const RatingItem()
-                ],
-              )
-            ],
+          const SizedBox(
+            width: 30,
           ),
-        )
-      ],
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.height * 0.3,
+                  child: const Text('Harry Potter and the order of the phoenix',
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      style: Styles.textStyle20),
+                ),
+                const Text('the author', style: Styles.textStyle14),
+                Row(
+                  children: [
+                    Text(
+                      '19.99 €',
+                      style: Styles.textStyle20
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    const Spacer(),
+                    const RatingItem()
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
